44d3808f1e3ccfc53f9ba33a315161fa69ec028e
Muhammad Khair <muhammad.rahmatul@mncgroup.com>
2021-10-23 10:44:21 +0700

    initial commit

--- GithubUser/app/consumergithubuser/src/main/java/com/reston/githubuser/consumergithubuser/view/interface/ProgressInterface.kt
+++
@@ -1,7 +0,0 @@
-package com.reston.githubuser.consumergithubuser.view.`interface`
-
-interface ProgressInterface {
-    fun onSucces()
-
-    fun onFailled()
-}
--- GithubUser/app/src/main/java/com/reston/githubuser/view/interface/ProgressInterface.kt
+++
@@ -1,7 +0,0 @@
-package com.reston.githubuser.view.`interface`
-
-interface ProgressInterface {
-    fun onSucces()
-
-    fun onFailled()
-}
